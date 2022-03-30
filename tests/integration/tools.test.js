a594bb399cfb252bbf84ebe5e1d04c8d9f8472be
jefranca <jean13franca@gmail.com>
2022-03-30 12:55:35 -0300

    :test_tube: test: GET /tools Done: responses 200 & :test_tube: DELETE /tools/:id Done: response 200 404

--- tests/integration/tools.test.js
+++ tests/integration/tools.test.js
@@ -2,9 +2,14 @@ import faker from "faker-br";
 import supertest from "supertest";
 
 import app from "../../src/app.js";
+import connection from "../../src/database.js";
 import "../../src/setup.js";
 
 const request = supertest(app);
+
+beforeAll(async () => {
+  await connection.query("TRUNCATE TABLE tags; TRUNCATE TABLE tools");
+});
 
 describe("POST /tools", () => {
   it("returns 201 for valid params", async () => {
@@ -12,7 +17,7 @@ describe("POST /tools", () => {
       title: faker.name.title(),
       link: faker.internet.url(),
       description: faker.lorem.text(),
-      tags: [faker.name.title(),faker.name.title()]
+      tags: [faker.name.title(), faker.name.title()],
     };
 
     const result = await request.post("/tools").send(body);
@@ -24,7 +29,7 @@ describe("POST /tools", () => {
       title: faker.name.title(),
       link: faker.internet.url(),
       description: faker.lorem.text(),
-      tgs: [faker.name.title(),faker.name.title()]
+      tgs: [faker.name.title(), faker.name.title()],
     };
 
     const result = await request.post("/tools").send(body);
@@ -32,23 +37,49 @@ describe("POST /tools", () => {
   });
 
   it("returns 409 for conflict", async () => {
-    const link=faker.internet.url();
+    const link = faker.internet.url();
     const body = {
       title: faker.name.title(),
       link: link,
       description: faker.lorem.text(),
-      tags: [faker.name.title(),faker.name.title()]
+      tags: [faker.name.title(), faker.name.title()],
     };
 
     const body2 = {
       title: faker.name.title(),
       link: link,
       description: faker.lorem.text(),
-      tags: [faker.name.title(),faker.name.title()]
+      tags: [faker.name.title(), faker.name.title()],
     };
-  
+
     await request.post("/tools").send(body);
     const result = await request.post("/tools").send(body2);
     expect(result.status).toEqual(409);
+  });
+});
+
+describe("GET /tools", () => {
+  it("returns 200", async () => {
+    const result = await request.get("/tools");
+
+    expect(result.status).toEqual(200);
+  });
+});
+
+describe("DELETE /tools/:id", () => {
+  it("returns 200 when deleted a tool", async () => {
+    const id = await connection.query(
+      "INSERT INTO tools (title,link,description) VALUES ('Notion','https://notion.so','All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized. ') RETURNING id"
+    );
+
+    const result = await request.delete(`/tools/${id.rows[0].id}`);
+
+    expect(result.status).toEqual(200);
+  });
+
+  it("returns 404 when deleted a tool", async () => {
+    const result = await request.delete("/tools/");
+
+    expect(result.status).toEqual(404);
   });
 });
